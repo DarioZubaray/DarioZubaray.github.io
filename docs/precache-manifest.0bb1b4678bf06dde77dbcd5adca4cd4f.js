7349d8e682ca9beb6b3c71de046a05374917558a
Dario Zubaray <dzubaray@gmail.com>
2021-03-09 10:33:50 -0300

    adding build as docs

---
+++ docs/precache-manifest.0bb1b4678bf06dde77dbcd5adca4cd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5923012fd2b1fd3346223e9844bf8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5ce1cfe5cecc90c05af",
+    "url": "/static/css/2.fc935826.chunk.css"
+  },
+  {
+    "revision": "25ef805f980083b654a9",
+    "url": "/static/css/main.9b13ff73.chunk.css"
+  },
+  {
+    "revision": "b5ce1cfe5cecc90c05af",
+    "url": "/static/js/2.493b225d.chunk.js"
+  },
+  {
+    "revision": "5bae6a6c19b45aed9a777adcc2ea7a32",
+    "url": "/static/js/2.493b225d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ef805f980083b654a9",
+    "url": "/static/js/main.42e6a8c3.chunk.js"
+  },
+  {
+    "revision": "f2e5371dbd7b5ab1fa21",
+    "url": "/static/js/runtime-main.9d03ed56.js"
+  }
+]);
