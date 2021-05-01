5d7a4e8280690a04f18311eba60aa299523dd859
Dario Zubaray <dzubaray@gmail.com>
2021-05-01 20:18:14 -0300

    fixing navbar for small devices

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,62 +1,81 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            <span role="img" aria-label="programmer">👨‍💻</span> Dario Zubaray
-          </Link>
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
 
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <NavLink 
-                    activeClassName="active"
-                    className="nav-item nav-link" 
-                    exact
-                    to="/"
-                >
-                    Home
-                </NavLink>
-              </li>
-              <li className="nav-item active">
-                <NavLink 
-                    activeClassName="active"
-                    className="nav-item nav-link" 
-                    exact
-                    to="/studies"
-                >
-                    Studies
-                </NavLink>
-              </li>
-              <li className="nav-item active">
-                <NavLink 
-                    activeClassName="active"
-                    className="nav-item nav-link" 
-                    exact
-                    to="/blog"
-                >
-                    Blog
-                </NavLink>
-              </li>
-              <li className="nav-item active">
-                <NavLink 
-                    activeClassName="active"
-                    className="nav-item nav-link" 
-                    exact
-                    to="/github"
-                >
-                    Github
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <span role="img" aria-label="programmer">
+            👨‍💻
+          </span>{" "}
+          Dario Zubaray
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active" onClick={handleNavCollapse}>
+              <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item active" onClick={handleNavCollapse}>
+              <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/studies"
+              >
+                Studies
+              </NavLink>
+            </li>
+            <li className="nav-item active" onClick={handleNavCollapse}>
+              <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/blog"
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item active" onClick={handleNavCollapse}>
+              <NavLink
+                activeClassName="active"
+                className="nav-item nav-link"
+                exact
+                to="/github"
+              >
+                Github
+              </NavLink>
+            </li>
+          </ul>
         </div>
-      </nav>
-    )
-}
+      </div>
+    </nav>
+  );
+};
--- src/components/ui/CourseFilter.js
+++ src/components/ui/CourseFilter.js
@@ -27,6 +27,7 @@ export const CourseFilter = ({ filterCourse }) => {
                 />
             ))
         }
+        <hr/>
     </>
   );
 };
