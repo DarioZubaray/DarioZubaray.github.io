e0f7cabb913253c9c00f103682ba9ce89b8522ac
Dario Zubaray <dzubaray@gmail.com>
2021-12-09 20:28:07 -0300

    updating blogcard images

--- src/components/ui/BlogCard.js
+++ src/components/ui/BlogCard.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
+import noImage from '../../static/blogger.png';
+import noInternetConnection from '../../static/no-internet.png';
 
 export const BlogCard = ({post}) => {
+    // TODO: add loader while checking 
+    // if there is intenet connection 
+    // and there is an image available in post.images object
+    const [connectionAvailable, setConnectionAvailable] = useState(false);
+    useEffect(() => {
+        setConnectionAvailable(navigator.onLine);
+    }, []);
+
+    const [imageAvailable, setImageAvailable] = useState(false);
+    useEffect(() => {
+        setImageAvailable(!!post.images[0]);
+    }, [setImageAvailable, post.images]);
+
+    console.log(`connection available: ${connectionAvailable}`);
+    console.log(`image available: ${imageAvailable}`);
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const handleOnLoad = () => {
+        console.log('handleOnLoad');
+        setIsLoaded(!isLoaded);
+    }
+
     return (
         <div className="col-md-4 mt-2 rounded">
             <div className="card h-100 rounded">
                 <div className="overflow">
                     {
-                        post.images[0] ? 
-                        ( <img src={post.images[0].url} alt={post.title} className="card-img-top rounded" /> )
-                        : (<img src="/static/img/blogger.png" alt="blogger post" className="card-img-top rounded" />)
+                        imageAvailable
+                        ? (
+                            connectionAvailable
+                            ? (
+                            <img
+                                src={post.images[0].url}
+                                alt={post.title}
+                                className="card-img-top rounded"
+                                onLoad={ handleOnLoad }
+                            />
+                            )
+                            : (<img
+                                src={noInternetConnection}
+                                alt="blogger post"
+                                className="card-img-top rounded" />)
+                        
+                        )
+                        : (<img
+                            src={noImage}
+                            alt="blogger post"
+                            className="card-img-top rounded" />)
                     }
                 </div>
                 <div className="card-body">
@@ -17,7 +59,11 @@ export const BlogCard = ({post}) => {
                     <p>{moment(post.published).fromNow()}</p>
                 </div>
                 <div className="card-footer">
-                    <a href={post.url} target="_blank" rel="noopener noreferrer" className="btn btn-outline-secondary btn-block">Read</a>
+                    <a
+                    href={post.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-outline-secondary btn-block">Read</a>
                 </div>
             </div>
         </div>
