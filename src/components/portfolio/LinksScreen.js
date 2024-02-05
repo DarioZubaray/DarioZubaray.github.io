b241950663e2a1c5910dee59afdc04fc20ece37d
Dario Zubaray <dzubaray@gmail.com>
2024-02-05 20:30:37 -0300

    feat: copy to clipboard url on links

--- src/components/portfolio/LinksScreen.js
+++ src/components/portfolio/LinksScreen.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { publicProfile } from '../../data/links';
 
 export const LinksScreen = () => {
+
+    const copyURLToClipboard = (url) => {
+        navigator.clipboard.writeText(url);
+    }
+
     return (
         <div className="container row mb-3 animate__animated animate__fadeIn">
             <div className="card" style={{ flex: 1 }}>
@@ -11,11 +16,17 @@ export const LinksScreen = () => {
                     <ul className="list-group">
                         {
                             publicProfile.map(pp => (
-                                <li className="list-group-item" key={ pp.nombre }>
+                                <li className="list-group-item"
+                                    style={{ display: "flex", justifyContent: "space-between" }}
+                                    key={ pp.nombre }>
                                     <a href={ pp.url } target="_blank" rel="noopener noreferrer">
                                         <span role="img" aria-label="emoji">{ pp.emoji }</span> 
                                         <span> { pp.nombre }</span>
                                     </a>
+
+                                    <button className="btn" onClick={() => copyURLToClipboard(pp.url)}>
+                                        <span role="img" aria-label="copy">📋</span>
+                                    </button>
                                 </li>
                             ))
                         }
