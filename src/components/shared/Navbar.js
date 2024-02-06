91c70fdfcf1fe5d1443dd0ac07127914f898c696
Dario Zubaray <dzubaray@gmail.com>
2024-02-06 15:45:25 -0300

    feat: update flowcv resume

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import pdf from '../../static/Dario-Zubaray-FlowCV-Resume-20240204.pdf'
+import pdf from '../../static/Dario-Zubaray-FlowCV-Resume-20240206.pdf'
 
 export const Navbar = () => {
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
