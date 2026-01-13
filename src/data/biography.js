33dc15d7b04a75c4ed2337a6aa536639f951e1d3
Dario Zubaray <dario.zubaray@botmaker.io>
2026-01-13 11:19:21 -0300

    update en cursos y emojis en biografia

--- src/data/biography.js
+++ src/data/biography.js
@@ -1,9 +1,12 @@
 export const biography = [
-  "I'm AGE years old ♑",
-  "father 👨‍👧",
-  "from argentina 🔵⚪🔵",
-  "a big fan of formula-1 🏎",
-  "and mates 🧉",
+  "I'm AGE years old ♑ 🐉",
+  "a father 👨‍👧, a bit of a gamer 🎮",
+  "a piano enthusiast 🎹",
+  "constantly training 🏋️‍♂️",
+  "a curious reader 📖",
+  "from Argentina 🔵⚪🔵",
+  "a big Formula 1 fan 🏎",
+  "and a mate lover 🧉",
 ];
 
 export const linkedinUrl = 'https://www.linkedin.com/in/dario-jose-zubaray';
