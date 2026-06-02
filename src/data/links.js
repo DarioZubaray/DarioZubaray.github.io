2305c2475ad9b89a72a0e2b669f59b06d513cef6
Dario Zubaray <dzubaray@gmail.com>
2026-06-02 15:43:15 -0300

    removing old links

--- src/data/links.js
+++ src/data/links.js
@@ -5,39 +5,9 @@ export const publicProfile = [
         nombre: 'Rick Astley - Never Gonna Give You Up (Official Music Video)'
     },
     {
-        url: 'https://www.udemy.com/user/dario-jose-zubaray/',
-        emoji: '🎓',
-        nombre: 'Udemy'
-    },
-    {
-        url: 'https://fernando-herrera.com/logros/#/dzubaray@gmail.com',
-        emoji: '👨‍🏫',
-        nombre: 'Fernando Herrera\'s web site'
-    },
-    {
         url: 'https://www.linkedin.com/in/dario-jose-zubaray/',
         emoji: '🛠️',
         nombre: 'LinkedIn'
-    },
-    {
-        url: 'https://calendly.com/dariozubaray/15min',
-        emoji: '📅',
-        nombre: 'Schedule a brief meeting/Agenda una breve reunión'
-    },
-    {
-        url: 'https://remoteok.com/@dariozubaray',
-        emoji: '🆗',
-        nombre: 'RemoteOK'
-    },
-    {
-        url: 'https://twitter.com/DarioZubaray',
-        emoji: '🐦',
-        nombre: 'Twitter/X'
-    },
-    {
-        url: 'https://flowcv.com/resume/huahl4r9ua',
-        emoji: '📜',
-        nombre: 'Flowcv'
     },
     {
         url: 'https://dar10comyr.blogspot.com/',
